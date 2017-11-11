17c322e01ef7d15e83f9cd6e7116aa612b8f6f7f
Szymon Bakunowicz <zorzin0os@gmail.com>
2017-11-11 17:28:45 +0100

    shops controller

---
+++ HomeWallet_API/Controllers/ShopsController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HomeWallet_API.Models;
+
+namespace HomeWallet_API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Shops")]
+    public class ShopsController : Controller
+    {
+        private readonly DBContext _context;
+
+        public ShopsController(DBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Shops/1
+        [HttpGet("{userId}")]
+        public IEnumerable<Shop> GetShops(int userId)
+        {
+            return _context.Shops.Where(s=>s.UserID == userId);
+        }
+
+        // GET: api/Shops/1/4
+        [HttpGet("{userId}/{id}")]
+        public async Task<IActionResult> GetShop(int userId, [FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var shop = await _context.Shops.SingleOrDefaultAsync(m => m.ID == id && m.UserID == userId);
+
+            if (shop == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(shop);
+        }
+
+        // PUT: api/Shops/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutShop([FromRoute] int id, [FromBody] Shop shop)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != shop.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(shop).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ShopExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Shops
+        [HttpPost]
+        public async Task<IActionResult> PostShop([FromBody] Shop shop)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Shops.Add(shop);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetShop", new { id = shop.ID }, shop);
+        }
+
+        // DELETE: api/Shops/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteShop([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var shop = await _context.Shops.SingleOrDefaultAsync(m => m.ID == id);
+            if (shop == null)
+            {
+                return NotFound();
+            }
+
+            _context.Shops.Remove(shop);
+            await _context.SaveChangesAsync();
+
+            return Ok(shop);
+        }
+
+        private bool ShopExists(int id)
+        {
+            return _context.Shops.Any(e => e.ID == id);
+        }
+    }
+}
